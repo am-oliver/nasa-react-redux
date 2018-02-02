6cb7195743317b68679933fae8dba64293248531
Abigail Oliver <aoliver@vendasta.com>
2018-02-01 23:21:57 -0600

    skeleton

---
+++ src/app/components/AstronomyContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+
+
+class AstronomyContainer extends Component {
+    render () {
+        return (
+            <div />
+        )
+    }
+}
+
+export default AstronomyContainer;
