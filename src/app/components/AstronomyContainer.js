4f4842ebabb9d14d2f57fb70befef57b0354c9ff
Abigail Oliver <aoliver@vendasta.com>
2018-02-02 00:09:30 -0600

    Nasa image loads, base react app

--- src/app/components/AstronomyContainer.js
+++ src/app/components/AstronomyContainer.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import AstronomyCard from './AstronomyCard';
 
 
 class AstronomyContainer extends Component {
+    constructor () {
+        super();
+        this.state = {
+            astronomy: []
+        }
+    }
+
+    componentDidMount() {
+        const NASA_URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+        const API_KEY = 'uW5oPgEqbCqQzts7H016GxKRnEo8bZuQrdfSRPPU';
+
+        axios.get(NASA_URL + API_KEY).then(resp => {
+            console.log(resp)
+            this.setState({
+                astronomy:resp.data
+            })
+        }).catch(err => {
+            console.log('errored: ', err)
+        });
+    }
+
     render () {
+        const { astronomy } = this.state;
         return (
-            <div />
+            <AstronomyCard data={astronomy} />
         )
     }
 }
